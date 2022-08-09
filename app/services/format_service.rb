58d2a080f2de28bb5db684c5c1d0e515a612e7c3
Gennadii Novoselov <gennady@novoselov.biz>
2022-08-09 12:56:14 -0500

    Refine divider algorithm

--- app/services/format_service.rb
+++ app/services/format_service.rb
@@ -20,15 +20,22 @@ class FormatService < ApplicationService
         finish_index ||= document.body.content.last.end_index
         chunk_size ||= params[:parts] ? (finish_index / params[:parts]).round : DEFAULT_CHUNK_SIZE
 
+        structural_element.paragraph.elements.each do |element|
+          # puts element.text_run.content
+          requests << update_background(element, :text_run, color_index) if element.text_run.present?
+        end
+
+        # puts "Chunk size: #{chunk_size}"
+        # puts "Part chunk: #{chunk_size * 0.35}"
+        # puts "Current chunk: #{structural_element.end_index - last_index}"
+        # puts "Next chunk: #{finish_index - structural_element.end_index}"
+
         if (structural_element.end_index - last_index >= chunk_size * 0.9) && (finish_index - structural_element.end_index > chunk_size * 0.35)
+          # puts "[SWITCH]"
           color_index += 1
           last_index = structural_element.end_index
         end
-
-        structural_element.paragraph.elements.each do |element|
-          # requests << update_background(element.inline_object_element, color_index) if element.inline_object_element.present?
-          requests << update_background(element, :text_run, color_index) if element.text_run.present?
-        end
+        # puts "----------------------------"
       end
     end
   end
