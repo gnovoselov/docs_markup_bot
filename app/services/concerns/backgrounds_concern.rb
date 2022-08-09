3d4eecd8ebe8f404f7198c4216fd86c7a33a969e
Gennadii Novoselov <gennady@novoselov.biz>
2022-08-09 11:26:56 -0500

    Adjust colors

--- app/services/concerns/backgrounds_concern.rb
+++ app/services/concerns/backgrounds_concern.rb
@@ -9,24 +9,24 @@ module BackgroundsConcern
     [1, 1, 1],          # white
     [1, 0.01, 1],       # fuchsia
     [0.29, 0.53, 0.9],  # light blue
-    [0.04, 1, 1],       # turquoise
     [0, 1, 0],          # green
-    [0.07, 0, 1],       # blue
+    [0.04, 1, 1],       # turquoise
     [1, 1, 0],          # yellow
     [0.6, 0, 1],        # purple
     [1, 0.6, 0],        # orange
     [1, 0, 0],          # red
-    [0.6, 0, 0],        # brown
-    [0.15, 0.3, 0.08],  # dark green
-    [0.05, 0.2, 0.24],  # teal
-    [0.04, 0.22, 0.39], # navy blue
     [0.85, 0.85, 0.85], # grey
     [0.84, 0.65, 0.74], # pink
-    [0.2, 0.11, 0.07],  # eggplant
-    [0.45, 0.11, 0.28], # plum
     [0.75, 0.56, 0.01], # beige
-    [0.71, 0.84, 0.66], # spring
+    [0.6, 0, 0],        # brown
+    [0.2, 0.11, 0.07],  # eggplant
+    [0.15, 0.3, 0.08],  # dark green
     [0.85, 0.82, 0.91], # lavander
+    [0.71, 0.84, 0.66], # spring
+    [0.07, 0, 1],       # blue
+    [0.05, 0.2, 0.24],  # teal
+    [0.45, 0.11, 0.28], # plum
+    [0.04, 0.22, 0.39], # navy blue
   ]
 
   private
