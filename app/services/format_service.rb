5f0fc50bfe28b2a28b55d1e827424b472d13824e
Gennadii Novoselov <gennady@novoselov.biz>
2022-08-11 00:32:00 -0500

    Refine dividing algorithm

--- app/services/format_service.rb
+++ app/services/format_service.rb
@@ -21,21 +21,22 @@ class FormatService < ApplicationService
         chunk_size ||= params[:parts] ? (finish_index / params[:parts]).round : DEFAULT_CHUNK_SIZE
 
         structural_element.paragraph.elements.each do |element|
-          # puts element.text_run.content
+          puts element.text_run.content
           requests << update_background(element, :text_run, color_index) if element.text_run.present?
         end
 
-        # puts "Chunk size: #{chunk_size}"
-        # puts "Part chunk: #{chunk_size * 0.35}"
-        # puts "Current chunk: #{structural_element.end_index - last_index}"
-        # puts "Next chunk: #{finish_index - structural_element.end_index}"
+        puts "Chunk size: #{chunk_size}"
+        puts "Little chunk: #{chunk_size - 100}"
+        puts "Part chunk: #{chunk_size * 0.35}"
+        puts "Current chunk: #{structural_element.end_index - last_index}"
+        puts "Next chunk: #{finish_index - structural_element.end_index}"
 
-        if (structural_element.end_index - last_index >= chunk_size * 0.9) && (finish_index - structural_element.end_index > chunk_size * 0.35)
-          # puts "[SWITCH]"
+        if (structural_element.end_index - last_index >= chunk_size - 100) && (finish_index - structural_element.end_index > chunk_size * 0.6)
+          puts "[SWITCH]"
           color_index += 1
           last_index = structural_element.end_index
         end
-        # puts "----------------------------"
+        puts "----------------------------"
       end
     end
 
