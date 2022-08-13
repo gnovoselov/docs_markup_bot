d987cc276f675236a8f8e84e9564c16641bc5719
Gennadii Novoselov <gennady@novoselov.biz>
2022-08-13 01:22:49 -0500

    Fix bundler for cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,9 @@
+env :PATH, ENV['PATH']
+set :bundle_command, "/usr/local/bin/bundle exec"
+
+set :environment, "production"
+set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
+
 every :minute do
   rake "divider:perform"
 end
