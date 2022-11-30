3561db16c1cea017b52f1b37d3b734d0fa766492
Gennadii Novoselov <gennady@novoselov.biz>
2022-11-29 22:25:12 -0600

    Fix clear service

--- app/services/clear_service.rb
+++ app/services/clear_service.rb
@@ -11,20 +11,20 @@ class ClearService < ApplicationService
   def call
     dept = 0
     change_document(params[:document_id]) do |document, structural_element, requests|
-      return unless structural_element.paragraph
+      if structural_element.paragraph
+        structural_element.paragraph.elements.each do |element|
+          next unless element.text_run.present?
 
-      structural_element.paragraph.elements.each do |element|
-        next unless element.text_run.present?
+          requests << update_background(element, :text_run, 0, -1 * dept)
 
-        requests << update_background(element, :text_run, 0, -1 * dept)
+          next if element.text_run.content.blank?
 
-        next if element.text_run.content.blank?
+          next if element.text_run&.content == DUPLICATED_LINK_CAPTION
 
-        next if element.text_run&.content == DUPLICATED_LINK_CAPTION
-
-        if WIP_OTHERS.match?(remove_non_ascii(element.text_run&.content))
-          requests << remove_range(element.start_index - dept, element.end_index - dept)
-          dept += element.end_index - element.start_index
+          if WIP_OTHERS.match?(remove_non_ascii(element.text_run&.content))
+            requests << remove_range(element.start_index - dept, element.end_index - dept)
+            dept += element.end_index - element.start_index
+          end
         end
       end
     end
