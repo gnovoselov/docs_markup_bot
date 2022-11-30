23524948b105babec9a43b352cd8253d034421f9
Gennadii Novoselov <gennady@novoselov.biz>
2022-11-29 21:43:38 -0600

    Fix clearing WIP marks

--- app/services/clear_service.rb
+++ app/services/clear_service.rb
@@ -11,30 +11,24 @@ class ClearService < ApplicationService
   def call
     dept = 0
     change_document(params[:document_id]) do |document, structural_element, requests|
-      process_structural_element(document, structural_element, requests, dept)
+      return unless structural_element.paragraph
+
+      structural_element.paragraph.elements.each do |element|
+        next unless element.text_run.present?
+
+        requests << update_background(element, :text_run, 0, -1 * dept)
+
+        next if element.text_run.content.blank?
+
+        next if element.text_run&.content == DUPLICATED_LINK_CAPTION
+
+        if WIP_OTHERS.match?(remove_non_ascii(element.text_run&.content))
+          requests << remove_range(element.start_index - dept, element.end_index - dept)
+          dept += element.end_index - element.start_index
+        end
+      end
     end
 
     "Документ очищен от меток"
-  end
-
-  private
-
-  def process_structural_element(_document, structural_element, requests, dept)
-    return unless structural_element.paragraph
-
-    structural_element.paragraph.elements.each do |element|
-      next unless element.text_run.present?
-
-      next if element.text_run.content.blank?
-
-      next if element.text_run&.content == DUPLICATED_LINK_CAPTION
-
-      if WIP_OTHERS.match?(remove_non_ascii(element.text_run&.content))
-        requests << remove_range(element.start_index - dept, element.end_index - dept)
-        dept += element.end_index - element.start_index
-      end
-
-      requests << update_background(element, :text_run, 0, -1 * dept)
-    end
   end
 end
