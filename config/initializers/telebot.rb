6ce5dc58005312e3770d9d7d3f3d11c44186a914
Gennadii Novoselov <gennady@novoselov.biz>
2022-12-13 18:51:49 -0600

    Remove ReggaeMortis from admins

--- config/initializers/telebot.rb
+++ config/initializers/telebot.rb
@@ -1,6 +1,6 @@
 TELEBOT_CONFIG = YAML::load_file(Rails.root.join('config', 'telebot.yml'))
 TELEBOT_HELP_MESSAGE = File.read(Rails.root.join('README.md'))
-TELEGRAM_ADMINS = %w[ReggaeMortis1 asternaks gnovoselov]
-TELEGRAM_ADMIN_CHATS = [36599423, 149074096, 153039812]
+TELEGRAM_ADMINS = %w[asternaks gnovoselov]
+TELEGRAM_ADMIN_CHATS = [149074096, 153039812]
 TELEGRAM_CHAT_URL = 'https://t.me/+_wpahFTDavE2ODUy'
 TELEGRAM_SUPPORT_CHAT = 153039812
