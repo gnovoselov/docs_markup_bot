0d743ee8a77eceed6323823eaa8087ea281dd2ed
Gennadii Novoselov <gennady@novoselov.biz>
2023-03-29 14:30:41 -0500

    Fix services to work with rvm

--- scripts/run_telebot.sh
+++ scripts/run_telebot.sh
@@ -2,4 +2,4 @@
 
 cd /root/docs_markup_bot
 
-/root/.rbenv/shims/bundle exec rake telebot:run RAILS_ENV=production
+/usr/local/rvm/rubies/ruby-3.1.2/bin/bundle exec rake telebot:run RAILS_ENV=production
