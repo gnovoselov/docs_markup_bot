61e26005d52fb0aaa8cb51af7996cfe9917e1f50
Gennadii Novoselov <gennady@novoselov.biz>
2023-03-29 15:04:45 -0500

    Move scripts back to rbenv/shims

--- scripts/run_telebot.sh
+++ scripts/run_telebot.sh
@@ -2,4 +2,4 @@
 
 cd /root/docs_markup_bot
 
-/usr/local/rvm/rubies/ruby-3.1.2/bin/bundle exec rake telebot:run RAILS_ENV=production
+/root/.rbenv/shims/bundle exec rake telebot:run RAILS_ENV=production
