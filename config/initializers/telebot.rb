55af209a653e3c2fdfe34555b0acb7b715cb86d8
Gennadii Novoselov <gennady@novoselov.biz>
2023-04-30 14:10:44 -0500

    Remove asternaks from admins

--- config/initializers/telebot.rb
+++ config/initializers/telebot.rb
@@ -1,6 +1,6 @@
 TELEBOT_CONFIG = YAML::load_file(Rails.root.join('config', 'telebot.yml'))
 TELEBOT_HELP_MESSAGE = File.read(Rails.root.join('README.md'))
-TELEGRAM_ADMINS = %w[asternaks gnovoselov anna_olga]
-TELEGRAM_ADMIN_CHATS = [149074096, 153039812, 5589849475]
+TELEGRAM_ADMINS = %w[gnovoselov anna_olga]
+TELEGRAM_ADMIN_CHATS = [153039812, 5589849475]
 TELEGRAM_CHAT_URL = 'https://t.me/+_wpahFTDavE2ODUy'
 TELEGRAM_SUPPORT_CHAT = 153039812
